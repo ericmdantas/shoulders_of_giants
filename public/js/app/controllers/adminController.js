19b10f5c7b019826ab09ececcd5d7e97b515db31
Eric Mendes Dantas <ericdantas0@hotmail.com>
2014-04-13 22:50:36 -0300

    changes

--- public/js/app/controllers/adminController.js
+++
@@ -1,9 +0,0 @@
-function AdminController($scope, $http)
-{
-    $scope.author = {};
-    $scope.addQuote = function(n,q)
-    {
-        $http.post('/postQuotes/'+n+'/'+q, $scope.author);
-        console.log('postaaaaaaang');
-    }
-}
--- public/js/app/controllers/quotesController.js
+++
@@ -1,9 +0,0 @@
-function QuotesController($scope, $http)
-{
-    $scope.quotes = undefined;
-
-    $http.get('/getQuotes').success(function(data)
-                                    {
-                                        $scope.quotes = data.quotes;
-                                    })
-}
