9d1fd8d42ad4db7c62f060fd609c7f6593f1c8b1
Eric Mendes Dantas <ericdantas0@hotmail.com>
2014-05-21 21:46:32 -0300

    .

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require('newrelic');
+
 var express         = require('express'),
     configurator    = require('./config/configurator'),
     db              = require('./config/dbase'),
